43e43ff6a069c296d7065bfdedbfb7f510cf6308
Zelys <140775870+zelys@users.noreply.github.com>
2023-09-23 22:06:42 -0300

    Update csv-to-sql.py

--- csv-to-sql.py
+++ csv-to-sql.py
@@ -1,5 +1,5 @@
 # funsion para definir los tipos de datos
-def definirDatos(filas):
+def definir_datos(filas):
     for indice in enumerate(filas):
         for dato in enumerate(indice[1]):
             if (
@@ -8,20 +8,30 @@ def definirDatos(filas):
                 or dato[1].lower() == "false"
             ):
                 filas[indice[0]][dato[0]] = dato[1]
+            elif dato[1].find("-") == 2:
+                filas[indice[0]][
+                    dato[0]
+                ] = f"'{dato[1][6:] + dato[1][2:6] + dato[1][0:2]}'"
             else:
-                filas[indice[0]][dato[False]] = f"'{dato[1]}'"
+                filas[indice[0]][dato[0]] = f"'{dato[1]}'"
     return filas
 
-# Abrir y leer archivo CSV
-with open("Medicos.csv", "r") as archivo_csv:
-    archivo = [linea.strip().split(";") for linea in archivo_csv]
 
-# concatenar lista e imprimir archivo SQL
-with open("insert_into.sql", "w") as insert_into:
-    insert_into.write(f"INSERT INTO ({', '.join(archivo.pop(0))})\nVALUE\n")
-    definirDatos(archivo)
-    for i in enumerate(archivo):
-        if i[0] == len(archivo) - 1:
-            insert_into.write(f"({', '.join(i[1])});")
-        else:
-            insert_into.write(f"({', '.join(i[1])}),\n")
+def procesar_archivo(nombre_archivo):
+    # Abrir y leer archivo CSV
+    with open(f"{nombre_archivo}.csv", "r") as archivo_csv:
+        archivo = [linea.strip().split(";") for linea in archivo_csv]
+
+    # concatenar lista e imprimir archivo SQL
+    with open(f"{nombre_archivo}.sql", "w") as insert_into:
+        insert_into.write(f"INSERT INTO ({', '.join(archivo.pop(0))})\nVALUE\n")
+        definir_datos(archivo)
+        for i in enumerate(archivo):
+            if i[0] == len(archivo) - 1:
+                insert_into.write(f"({', '.join(i[1])});")
+            else:
+                insert_into.write(f"({', '.join(i[1])}),\n")
+
+
+analizar = input("Ingresa el nombre del archivo CSV: ")
+procesar_archivo(analizar)
